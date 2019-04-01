c457cc8b2b20d4d3ba7db17617ccab0d3db428db
KirilPopovski <48929770+KirilPopovski@users.noreply.github.com>
2019-04-01 22:36:45 +0300

    LambdaExpressions

---
+++ LambdaAndLinq/LambdaAndLinq/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LambdaAndLinq
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var str = new StringBuilder();
+            str.Append(Console.ReadLine());
+            Console.WriteLine(str.Substring(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+        }
+        
+    }
+    public static class StringExtension
+    {
+        public static StringBuilder Substring(this StringBuilder str,int index,int lenght)
+        {
+            var str2 = new StringBuilder();
+            for(int i=index;i<index+lenght;i++)
+            {
+                str2.Append(str[i]);
+            }
+            return str2;
+        }
+    }
+}
---
+++ LambdaAndLinq2/LambdaAndLinq2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaAndLinq2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student("kiril", "popovski", 20));
+            students.Add(new Student("anna", "brezova", 20));
+            students.Add(new Student("sasho", "dimitrov", 20));
+            students.Add(new Student("petq", "tancheva", 20));
+            foreach(var item in students.Where(x=>x.Age>=18 && x.Age<=19))
+            {
+                Console.WriteLine(item.lastName + item.Name + item.Age.ToString());
+            }
+        }
+    }
+    public class Student
+    {
+        public string Name { get; set; }
+        public string lastName { get; set; }
+        public int Age { get; set; }
+        public Student(string name,string lastname,int age)
+        {
+            this.Name = name;
+            this.lastName = lastname;
+            this.Age = age;
+        }
+    }
+}
