8eea8ad58571b97497e8e731977f598fd6935a76
KirilPopovski <48929770+KirilPopovski@users.noreply.github.com>
2019-04-01 22:32:15 +0300

    dictionaries

---
+++ dictionaries-part2/dictionaries-part2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dictionaries_part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var starwars = new Dictionary<string, string>();
+            string input = Console.ReadLine();
+            while (input != "Lumpawaroo")
+            {
+                if (input.Contains('|'))
+                {
+                    string[] parsed = input.Split(" | ");
+                    if (!starwars.ContainsKey(parsed[1]))
+                    {
+                        starwars[parsed[1]] = parsed[0];
+                    }
+                }
+                else if (input.Contains('>'))
+                {
+                    string[] parsed = input.Split(" -> ");
+                    if (starwars.ContainsKey(parsed[0]))
+                    {
+                        starwars[parsed[0]] = parsed[1];
+                        Console.WriteLine("{0} joins the {1} side!", parsed[0], parsed[1]);
+                    }
+                    else
+                    {
+                        starwars[parsed[0]] = parsed[1];
+                        Console.WriteLine("{0} joins the {1} side!", parsed[0], parsed[1]);
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            var counts = new Dictionary<string, List<string>>();
+            foreach(var item in starwars)
+            {
+                if (!counts.ContainsKey(item.Value))
+                {
+                    counts[item.Value] = new List<string>();
+                }
+                counts[item.Value].Add(item.Key);
+            }
+            foreach(var item in counts.OrderByDescending(x=>x.Value.Count).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine("Side: {0}, Members: {1}", item.Key, item.Value.Count);
+                foreach(var item2 in item.Value.OrderBy(x=>x))
+                {
+                    Console.WriteLine("! {0}", item2);
+                }
+            }
+        }
+    }
+}
---
+++ dictionaries/dictionaries/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new Dictionary<string, List<double>>();
+            int n = int.Parse(Console.ReadLine());
+            for(int i=0;i<n;i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+                if(!students.ContainsKey(name))
+                {
+                    students[name] = new List<double>();
+                }
+                students[name].Add(grade);
+            }
+            foreach(var item in students.OrderByDescending(x=>x.Value.Average()))
+            {
+                if (item.Value.Average() >= 4.5) Console.WriteLine("{0} -> {1:F2}", item.Key, item.Value.Average());
+            }
+        }
+    }
+}
